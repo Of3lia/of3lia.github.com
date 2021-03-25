823024f31cc03db361e7b61491d35818f5e94646
Ofelia <ofeliacocucci@outlook.com>
2021-03-25 10:39:35 +0100

    project1

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { StudiesComponent } from './studies/studies.component';
+import { ExperienceComponent } from './experience/experience.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent,
+    StudiesComponent,
+    ExperienceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
